241ef3a530ee574612fe5631bc99e05d1dc10497
Daniel Sanabria <sanabriabrasil@hotmail.com>
2021-10-16 10:00:01 -0300

    DEV - Add route to Post transaction, route.swift

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -7,4 +7,5 @@ func routes(_ app: Application) throws {
     let blockchain = BlockchainController()
     let blockchainApp = app.grouped("blockchain")
     blockchainApp.get("", use: blockchain.getBlockchain)
+    blockchainApp.post("transaction", use: blockchain.transaction)
 }
